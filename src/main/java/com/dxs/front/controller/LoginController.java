c266b679da4b4231ccd01c1e84ceecad46e9d65d
Doris <626462094@qq.com>
2020-03-29 15:56:57 +0800

    add sql ddl

--- src/main/java/com/dxs/front/controller/LoginController.java
+++ src/main/java/com/dxs/front/controller/LoginController.java
@@ -1,13 +1,19 @@
 package com.dxs.front.controller;
 
+import com.dxs.front.mapper.UserMapper;
 import com.dxs.front.pojo.AccessTokenDTO;
 import com.dxs.front.pojo.GithubUser;
+import com.dxs.front.pojo.User;
 import com.dxs.front.service.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class LoginController {
@@ -21,9 +27,13 @@ public class LoginController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
-                           @RequestParam(name = "state")String state){
+                           @RequestParam(name = "state")String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirectUri);
@@ -33,8 +43,25 @@ public class LoginController {
 
         //通过accessToken获取用户信息
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+//        System.out.println(githubUser.getName());
+        if(githubUser!=null){
+            //获取登录信息
+            User user = new User();
+            //UUID防止id重复
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtModified());
+            userMapper.insert(user);
+
+            //登录成功，写cookie和session
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else {
+            return "redirect:/";
+        }
+
     }
 }
